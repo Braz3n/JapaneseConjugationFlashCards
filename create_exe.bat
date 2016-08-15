128b570267d6aef6031359867537f99fff7f091e
Zane Barker <zane.barker@gmail.com>
2016-08-15 22:33:46 +1200

    create_exe.bat now copies the Verbs and Adjectives directory into the dist directory where the executable is created.

--- create_exe.bat
+++ create_exe.bat
@@ -1,1 +1,3 @@
 pyinstaller gui.py --onefile --noconsole --name ConjugationFlashCards --clean -p C:\Users\Zane\AppData\Local\Programs\Python\Python35\Lib\site-packages\PyQt5\Qt\bin
+robocopy Verbs dist\Verbs
+robocopy Adjectives dist\Adjectives
