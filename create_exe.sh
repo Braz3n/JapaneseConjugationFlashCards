a1c8a0a33b4824c6fb9b547be0407235439aff3e
Zane Barker <zane.barker@gmail.com>
2016-08-21 23:21:04 +1200

    Fixed bug in create_exe.sh

--- create_exe.sh
+++ create_exe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 pyinstaller gui.py --onefile --noconsole --name ConjugationFlashCards --clean -p /Users/zane/python_virtual/pyinstaller-py3.5/lib/python3.5/site-packages/
-cp -R ./verbs ./dist/verbs
-cp -R ./adjectives ./dist/adjectives
+cp -R ./verbs ./dist
+cp -R ./adjectives ./dist
 # Remove the standalone executable.
 rm ./dist/ConjugationFlashCards
 # Add an entry into the .plist file so that PyQt5 makes the most of a Retina display.
